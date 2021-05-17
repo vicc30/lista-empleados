e518bd28b43bc3c1a174f2e2cd9facee9cf49ae3
Victor Cruz <vicc23@gmail.com>
2021-05-17 14:22:20 -0500

    Layout general

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { Table, InputGroup, FormControl, Button, ButtonGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container-fluid">
+        <h1 className="text-center">Lista de Empleados</h1>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Buscar Empleado o Empresa"
+            aria-label="Buscar"
+            aria-describedby="buscar"
+          />
+          <InputGroup.Append>
+            <Button variant="outline-secondary">Buscar</Button>
+          </InputGroup.Append>
+        </InputGroup>
+        <ButtonGroup>
+          <Button primary>Currency: MXN</Button>
+          <Button disabled>Total empleados: 2</Button>
+        </ButtonGroup>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nombre</th>
+              <th>Empresa</th>
+              <th>Salario</th>
+              <th>Imagen</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Victor</td>
+              <td>empresa 1</td>
+              <td>$18,000</td>
+              <td>Img</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Victor</td>
+              <td>empresa 1</td>
+              <td>$18,000</td>
+              <td>Img</td>
+            </tr>
+          </tbody>
+        </Table>
+        <button className="btn btn-primary">Nuevo Empleado</button>
+        <button className="btn btn-secondary ml-md-2">Editar Empleado</button>
+      </section>
+    </>
   );
 }
 
